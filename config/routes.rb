75a788e5823c89f9a1f2c714b1270206d3be3c86
yumoto34492 <78357097+yumoto34492@users.noreply.github.com>
2021-02-25 11:21:14 +0900

    ルーティングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
     root to: 'items#index'
-    resources :users, only: [:show]
 end
