2453fdea7e997b6476eb48aebe3283a0ea2b708b
yumoto34492 <78357097+yumoto34492@users.noreply.github.com>
2021-02-25 18:55:12 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
-class Item < ApplicationRecord
+class Item < ActiveHash::Base
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to [:category, :condition, :days_to_ship, :delivery_fee, :prefecture]
 end
