e3ca5651d4998efdf3ba117ab83f379e80c2c6f9
yumoto34492 <78357097+yumoto34492@users.noreply.github.com>
2021-02-25 11:35:55 +0900

    バリデーションにwith_optionsの設定追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters'}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters'}
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/,  message: 'Full-width katakana characters'}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/,  message: 'Full-width katakana characters'}
-  validates :birthday, presence: true
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers'}
+  with_options presence: true do
+    validates :nickname 
+    validates :birthday
+      with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters'} do
+        validates :last_name
+        validates :first_name
+      end
+      with_options format: { with: /\A[ァ-ヶ]+\z/,  message: 'Full-widthatakana characters'} do
+        validates :last_name_kana
+        validates :first_name_kana
+      end
+    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers'}
+    end
 end
