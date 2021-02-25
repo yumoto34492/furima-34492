65cbf96b4754767f08938378be0553c00b6631ce
yumoto34492 <78357097+yumoto34492@users.noreply.github.com>
2021-02-25 19:00:48 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ActiveHash::Base
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to [:category, :condition, :days_to_ship, :delivery_fee, :prefecture]
+
+  validates :category, :condition, :days_to_ship, :delivery_fee, :prefecture, presence: true
+  validates :category_id, :condition_id, :days_to_ship_id, :delivery_fee_id, :prefecture_id, numericality: { other_than: 1 } 
 end
