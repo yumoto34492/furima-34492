a93f1b5fdb5a5ae3f9816cd5114974b9940d579d
yumoto34492 <78357097+yumoto34492@users.noreply.github.com>
2021-03-01 12:29:09 +0900

    items とのアソシエーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :items
+
   with_options presence: true do
     validates :nickname
     validates :birthday
