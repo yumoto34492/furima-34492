9186cd120083a0b3c4b77a58eb536b902b12b52c
yumoto34492 <78357097+yumoto34492@users.noreply.github.com>
2021-03-02 19:38:32 +0900

    出品者のみ編集可能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,6 +31,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
