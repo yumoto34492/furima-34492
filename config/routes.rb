b35cf5798db8212aecdf5706b7960094cd5b283e
yumoto34492 <78357097+yumoto34492@users.noreply.github.com>
2021-03-07 18:10:02 +0900

    購入のためのフォームを実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
     root to: 'items#index'
     resources :items do
-      resources :orders, only: [:index]
+      resources :orders, only: [:index, :create]
     end
 
 end
