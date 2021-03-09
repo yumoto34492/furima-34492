6e3480fdcd16aef56e8a30e7c5645b8d70f2c71a
yumoto34492 <78357097+yumoto34492@users.noreply.github.com>
2021-03-09 16:27:32 +0900

    tokenをサーバーサイドへ送信

---
+++ app/javascript/order.js
@@ -0,0 +1,28 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_f112c62e10d86ad46bae2951");
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+      const formResult = document.getElementById("charge-form");
+      const formData = new FormData(formResult);
+
+      const card = {
+        number: formData.get("purchases_shipping_address[number]"),
+        exp_month: formData.get("purchases_shipping_address[exp_month]"),
+        exp_year: `20${formData.get("purchases_shipping_address[exp_year]")}`,
+        cvc: formData.get("purchases_shipping_address[cvc]"),
+      };
+
+      Payjp.createToken(card, (status, response) => {
+        if (status == 200 ) {
+          const token = response.id;
+          const renderDom = document.getElementById("charge-form");
+          const tokenObj = `<input value=${token} name='token' type="hidden">`;
+          renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        }
+      });
+  });
+};
+
+window.addEventListener("load", pay);
