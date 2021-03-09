a993b29ccbd5ec59d57e5a437c4d2d164765f325
yumoto34492 <78357097+yumoto34492@users.noreply.github.com>
2021-03-09 16:36:52 +0900

    クレジットカード情報を削除

--- app/javascript/order.js
+++ app/javascript/order.js
@@ -21,6 +21,11 @@ const pay = () => {
           const tokenObj = `<input value=${token} name='token' type="hidden">`;
           renderDom.insertAdjacentHTML("beforeend", tokenObj);
         }
+
+      document.getElementById("purchases_shipping_address_number").removeAttribute("name");
+      document.getElementById("purchases_shipping_address_exp_month").removeAttribute("name");
+      document.getElementById("purchases_shipping_address_exp_year").removeAttribute("name");
+      document.getElementById("purchases_shipping_address_cvc").removeAttribute("name");
       });
   });
 };
