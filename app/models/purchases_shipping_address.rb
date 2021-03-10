b91a981037649d39ce760001d2f864061ac9ef8c
yumoto34492 <78357097+yumoto34492@users.noreply.github.com>
2021-03-10 16:13:31 +0900

    バリデーションの追加

--- app/models/purchases_shipping_address.rb
+++ app/models/purchases_shipping_address.rb
@@ -8,6 +8,8 @@ class PurchasesShippingAddress
     validates :address
     validates :phone_num, length: { maximum: 11 }, format: {with: /\A[0-9]+\z/}
     validates :token
+    validates :user_id
+    validates :item_id
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
