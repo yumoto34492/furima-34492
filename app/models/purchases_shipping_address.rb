69527bb98874bd5e157f2d12eb2c2a3464902e63
yumoto34492 <78357097+yumoto34492@users.noreply.github.com>
2021-03-10 16:06:44 +0900

    バリデーションの訂正

--- app/models/purchases_shipping_address.rb
+++ app/models/purchases_shipping_address.rb
@@ -2,12 +2,12 @@ class PurchasesShippingAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_num, :token
 
-  validates :token, presence: true
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :municipality
     validates :address
     validates :phone_num, length: { maximum: 11 }, format: {with: /\A[0-9]+\z/}
+    validates :token
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
